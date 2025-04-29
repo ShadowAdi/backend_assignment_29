540c01ff99a3ad86efe16cb2cfe130ce2aa53268
Aditya Shukla <Shadowshukla76@gmail.com>
2025-04-29 21:59:13 +0530

    add code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import  router  from "./routes/router.js";
+import router from "./routes/router.js";
 import cors from "cors";
 import { DBConnect } from "./utils/db.js";
 import { configDotenv } from "dotenv";
@@ -9,7 +9,10 @@ configDotenv()
 
 export const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: "https://frontend-n74wu67c5-adityas-projects-32315b04.vercel.app",
+    credentials: true
+}));
 app.use(express.json())
 app.use("/api", router)
 
